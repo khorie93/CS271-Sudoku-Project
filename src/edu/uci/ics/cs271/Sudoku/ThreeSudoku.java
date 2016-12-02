857ff3da8c8ced4927155db421f3d2d15410eb55
khorie93 <khorie@uci.edu>
2016-12-01 19:03:28 -0800

    initialize and outputInitialize
    made codes the reverse of the other

---
+++ src/edu/uci/ics/cs271/Sudoku/ThreeSudoku.java
@@ -0,0 +1,145 @@
+package edu.uci.ics.cs271.Sudoku;
+
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintStream;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class ThreeSudoku 
+{
+    Integer[][] puzzle = new Integer[9][9];
+    
+    public void initialize(InputStream in)
+    {
+        int value, rowVal;
+        Scanner input = new Scanner(in);
+        
+        for (int i = 0; i < 81; i++)
+        {
+            value = input.nextInt();
+            if (value < 0)
+            {
+                throw new InputMismatchException("Input value is less than 0.");
+            }
+            if (value > 9)
+            {
+                throw new InputMismatchException("Input value is greater than 9.");
+            }
+            rowVal = (int) Math.floor(i/9);
+            if (rowVal == 0)
+            {
+                if (value == 0)
+                {
+                    puzzle[rowVal][i] = null;
+                }
+                else
+                {
+                    puzzle[rowVal][i] = value;
+                }
+            }
+            if (rowVal == 1)
+            {
+                if (value == 0)
+                {
+                    puzzle[rowVal][i-9] = null;
+                }
+                else
+                {
+                    puzzle[rowVal][i-9] = value;
+                }
+            }
+            if (rowVal == 2)
+            {
+                if (value == 0)
+                {
+                    puzzle[rowVal][i-18] = null;
+                }
+                else
+                {
+                    puzzle[rowVal][i-18] = value;
+                }
+            }
+            if (rowVal == 3)
+            {
+                if (value == 0)
+                {
+                    puzzle[rowVal][i-27] = null;
+                }
+                else
+                {
+                    puzzle[rowVal][i-27] = value;
+                }
+            }
+            if (rowVal == 4)
+            {
+                if (value == 0)
+                {
+                    puzzle[rowVal][i-36] = null;
+                }
+                else
+                {
+                    puzzle[rowVal][i-36] = value;
+                }
+            }
+            if (rowVal == 5)
+            {
+                if (value == 0)
+                {
+                    puzzle[rowVal][i-45] = null;
+                }
+                else
+                {
+                    puzzle[rowVal][i-45] = value;
+                }
+            }
+            if (rowVal == 6)
+            {
+                if (value == 0)
+                {
+                    puzzle[rowVal][i-54] = null;
+                }
+                else
+                {
+                    puzzle[rowVal][i-54] = value;
+                }
+            }
+            if (rowVal == 7)
+            {
+                if (value == 0)
+                {
+                    puzzle[rowVal][i-63] = null;
+                }
+                else
+                {
+                    puzzle[rowVal][i-63] = value;
+                }
+            }
+            if (rowVal == 8)
+            {
+                if (value == 0)
+                {
+                    puzzle[rowVal][i-72] = null;
+                }
+                else
+                {
+                    puzzle[rowVal][i-72] = value;
+                }
+            }
+        }
+    }
+    
+    public void outputInitialize(OutputStream out)
+    {
+        PrintStream output = new PrintStream(out);
+        
+        for (int i = 0; i < 9; i++)
+        {
+            for (int j = 0; j < 9; j++)
+            {
+                output.print(puzzle[i][j] + " ");
+            }
+            output.println();
+        }
+    }
+}
--- src/edu/uci/ics/cs271/SudokuSolver/InitialBoard.java
+++ src/edu/uci/ics/cs271/SudokuSolver/InitialBoard.java
@@ -1,124 +1,23 @@
 package edu.uci.ics.cs271.SudokuSolver;
 
-import java.util.Scanner;
-import java.lang.Integer;
-import java.lang.Math;
+import edu.uci.ics.cs271.Sudoku.ThreeSudoku;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.OutputStream;
+import static java.lang.System.out;
+
 
 
 public class InitialBoard 
 {
-    public static void main(String[] args)
+    public static void main(String[] args) throws FileNotFoundException
     {
-        int value, rowVal;
-        Integer[][] puzzle = new Integer[9][9];
-        Scanner keyboard = new Scanner(System.in);
+        ThreeSudoku puzz = new ThreeSudoku();
         System.out.println("Enter sequence of 81 integers ranging [0,9], "
                 + "encoding the initial board position, left-to-right and top"
                 + "-down, with 0 for empty squares:");
-        
-        for (int i = 0; i < 81; i++)
-        {
-            value = keyboard.nextInt();
-            rowVal = (int) Math.floor(i/9);
-            if (rowVal == 0)
-            {
-                if (value == 0)
-                {
-                    puzzle[rowVal][i] = null;
-                }
-                else
-                {
-                    puzzle[rowVal][i] = value;
-                }
-            }
-            if (rowVal == 1)
-            {
-                if (value == 0)
-                {
-                    puzzle[rowVal][i-9] = null;
-                }
-                else
-                {
-                    puzzle[rowVal][i-9] = value;
-                }
-            }
-            if (rowVal == 2)
-            {
-                if (value == 0)
-                {
-                    puzzle[rowVal][i-18] = null;
-                }
-                else
-                {
-                    puzzle[rowVal][i-18] = value;
-                }
-            }
-            if (rowVal == 3)
-            {
-                if (value == 0)
-                {
-                    puzzle[rowVal][i-27] = null;
-                }
-                else
-                {
-                    puzzle[rowVal][i-27] = value;
-                }
-            }
-            if (rowVal == 4)
-            {
-                if (value == 0)
-                {
-                    puzzle[rowVal][i-36] = null;
-                }
-                else
-                {
-                    puzzle[rowVal][i-36] = value;
-                }
-            }
-            if (rowVal == 5)
-            {
-                if (value == 0)
-                {
-                    puzzle[rowVal][i-45] = null;
-                }
-                else
-                {
-                    puzzle[rowVal][i-45] = value;
-                }
-            }
-            if (rowVal == 6)
-            {
-                if (value == 0)
-                {
-                    puzzle[rowVal][i-54] = null;
-                }
-                else
-                {
-                    puzzle[rowVal][i-54] = value;
-                }
-            }
-            if (rowVal == 7)
-            {
-                if (value == 0)
-                {
-                    puzzle[rowVal][i-63] = null;
-                }
-                else
-                {
-                    puzzle[rowVal][i-63] = value;
-                }
-            }
-            if (rowVal == 8)
-            {
-                if (value == 0)
-                {
-                    puzzle[rowVal][i-72] = null;
-                }
-                else
-                {
-                    puzzle[rowVal][i-72] = value;
-                }
-            }
-        }
-    }
+        puzz.initialize(new FileInputStream("/Users/kanabee/NetBeansProjects/Sudoku/build/classes/Input.txt"));
+        System.out.println();
+        puzz.outputInitialize(System.out);
+    }   
 }
